30d1814eae3939caefd2906e35c0eacb3d0ea92e
Duda <maria.avila.januario@escola.pr.gov.br>
2026-04-16 11:54:14 -0300

    Delete 1TRIMESTRE/Aula0 directory

--- 1TRIMESTRE/Aula0/Exercício02.py
+++
@@ -1,9 +0,0 @@
-Palavra = input("Digite algo: ")
-Palavra = str(Palavra)
-print("A quantidade de caracteres é: ",(len(Palavra)))
-
-cont = 0
-for L in Palavra:
-    if L=="a":
-        cont=cont+1
-print("A quantidade de letras A é: ",cont)
