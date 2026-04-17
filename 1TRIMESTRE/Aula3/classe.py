d6abf82b7a09ff5fe4f396ba1af27bef1cce20fc
Duda <maria.avila.januario@escola.pr.gov.br>
2026-04-16 23:49:50 -0300

    Add files via upload

--- 1TRIMESTRE/Aula3/classe.py
+++ 1TRIMESTRE/Aula3/classe.py
@@ -9,11 +9,11 @@ class Serie:
 
         def like(self):
             self.nota += 0.2
-            if self.nota>5:
+            if self.nota > 5:
                 self.nota = 5
         def dislike(self):
             self.nota -= 0.2
-            if self.nota<0:
+            if self.nota < 0:
                 self.nota = 0
 
 catalogo = [ ]
@@ -26,16 +26,17 @@ catalogo.append(Serie("Uma Mente Excepcional", "fantasia", 37, 3.8))
 
 while(True):
     comando = input("""Digite a função:
-    1 - Ver séries cadastradas
+    1 - Ver series cadastradas
     2 - Procurar serie por genero
-    3 - Mostrar séries favoritas
+    3 - Mostrar series favoritas
     4 - Dar like em uma serie
     5 - Cadastrar nova serie
     0 - Sair
     """)
-
+    
     match comando:
         case "1":
+            print()
             for serie in catalogo:
                 print(f"#{serie.indice} Nome: {serie.nome} / Genero: {serie.genero} / n° de episodios: {serie.episodios} / Nota: {serie.nota}")
             print()
@@ -46,19 +47,54 @@ while(True):
             for serie in catalogo:
                 if (serie.genero == opção):
                     print(f"#{serie.indice} Nome: {serie.nome} / Genero: {serie.genero} / n° de episodios: {serie.episodios} / Nota: {serie.nota}")
-
-
-
-
+            print()
 
         case "3":
-            print("Mostrar series favoritas")
+            print()
+            for serie in catalogo:
+                if (serie.nota > 4.0):
+                    print(f"#{serie.indice} Nome: {serie.nome} / Genero: {serie.genero} / n° de episodios: {serie.episodios} / Nota: {serie.nota}")
+            print()
+
         case "4":
-            print("Dar like")
+            print()
+            indice = int(input("Qual série você gostaria de avaliar? Digite o índice: "))
+            avaliacao = input("like ou dislike? ")
+                
+            if avaliacao == "like":
+                serie.like()
+                print(f"Você deu dislike em '{serie.nome}'! Nova nota: {serie.nota}")
+
+            elif avaliacao == "dislike":
+                serie.dislike()
+                print(f"Você deu dislike em '{serie.nome}'! Nova nota: {serie.nota}")
+            
+            print("Obrigada pela sua avaliação!")
+            print()
+
+            
+
         case "5":
-            print("Cadastrar serie")
+            novo_indice = len(catalogo)
+            print()
+            nome = input("Nome da série: ")
+            genero = input("Gênero: ")
+            episodios = int(input("Número de episódios: "))
+            nota = float(input("Nota (0 a 5): "))
+            print()
+            
+            nova_serie = Serie(nome, genero, episodios, nota)
+            catalogo.append(nova_serie)
+
+            print(f"Série {nome} cadastrada com sucesso!")
+            print()
+            for serie in catalogo:
+                print(f"#{serie.indice} Nome: {serie.nome} / Genero: {serie.genero} / n° de episodios: {serie.episodios} / Nota: {serie.nota}")
+            print()
+
         case "0":
             print("Fim do programa")
             break
+
         case _:
             print("Valor inválido")
